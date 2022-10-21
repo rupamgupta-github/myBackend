a6df3d989ee28ca8adc584827c4d7b94a14d9163
rupamgupta-github <guptarupam.1020@gmail.com>
2022-10-21 18:46:28 +0530

    Mongo DB Connection Successfully created

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
-const express = require('express');
-var bodyParser = require('body-parser');
+const express = require("express");
+var bodyParser = require("body-parser");
 
-const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+const route = require("./routes/route.js");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
-, {
-   useNewUrlParser: true 
-}
-).then( () => {console.log( "MongoDb is connected")}  )
-.catch( err => console.log(err))
+mongoose
+  .connect(
+    "mongodb+srv://mr_rupam:uttam@cluster0.eaxynkv.mongodb.net/Rupam1-DB?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => console.log(err));
 
+app.use("/", route);
 
-
-
-app.use('/', route);
-
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 3000));
 });
-
