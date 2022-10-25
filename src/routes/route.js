f1d26f0a300300f23c77946c701cd9bad82b14bd
Rupam (Dev) Gupta <69069396+rupamgupta-github@users.noreply.github.com>
2022-10-25 15:44:57 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,17 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
+router.post("/createbook",async (req,res)=>{
+    let data=req.body;
+    let saveData=await bookModel.create(data);
+    res.send({msg:saveData})
+  })
+  
+  router.get("/getbook",async (req,res)=>{
+    let allBook=await bookModel.find()
+    res.send({msg:allBook})
+  })
+
 router.post("/createUser", UserController.createUser  )
 
 router.get("/getUsersData", UserController.getUsersData)
